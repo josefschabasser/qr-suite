469f4fbcb8a3bb2f19fb182d7e171bc62648d628
Josef Schabasser <josef.schabasser@salamantex.com>
2021-04-19 23:50:57 +0200

    add documentation to encode.ts, add documentation to `EncodedData` interface and rename props, move versions from main to datatypes

--- packages/matrix/src/encode.ts
+++ packages/matrix/src/encode.ts
@@ -1,41 +1,78 @@
 import { EncodedData, Encoding } from './datatypes'
 
+/**
+ * Encodes a character and pushes the results onto an array.
+ * @param {number[]} arr The target array to push onto.
+ * @param {number} size The size of the charater to encode.
+ * @param {number} value The charater to encode.
+ */
 function pushBits(arr: number[], size: number, value: number): void {
   for (let bit = 1 << (size - 1); bit; bit >>>= 1) {
     arr.push(bit & value ? 1 : 0)
   }
 }
 
+/**
+ * Packs encoded data into an element containing encoding and length information.
+ * The resulting array is `[Encoding:4] [Length:variable] [Data:variable]`
+ * @param {number[]} encoding The encoding to use.
+ * @param {number} length The length of the data.
+ * @param {number} size The size of one character.
+ * @param {number[]} bits The data to work on.
+ * @returns {number[]} The packed data, encoding and length information.
+ */
 function getData(encoding: number[], length: number, size: number, bits: number[]): number[] {
   const d = encoding.slice()
   pushBits(d, size, length)
   return d.concat(bits)
 }
 
-function encode8Bit(data: Buffer): EncodedData {
+/**
+ * Encodes 8bit binary data.
+ * @param {Buffer} data Binary data to encode, 8 bits/character.
+ * @returns {EncodedData} The encoded data for all versions.
+ */
+function encode8Bit(data: Buffer | Array<number>): EncodedData {
   const encoding = Encoding.Byte
   const length = data.length
   const bits: number[] = []
-  const result = (): EncodedData => ({ data1: [], data10: [], data27: [] })
+  const result = (): EncodedData => ({
+    DataVersionLow: [],
+    DataVersionMid: [],
+    DataVersionHigh: [],
+  })
 
+  // encode characters one by one, 8 bits/character
   for (let i = 0; i < length; i++) {
     pushBits(bits, 8, data[i])
   }
 
-  result.data10 = result.data27 = getData(encoding, length, 16, bits)
+  result.DataVersionHigh = getData(encoding, length, 16, bits)
+  result.DataVersionMid = result.DataVersionHigh
   if (length < 256) {
-    result.data1 = getData(encoding, length, 8, bits)
+    result.DataVersionLow = getData(encoding, length, 8, bits)
   }
   return result
 }
 
+/**
+ * Encodes alphanumeric data. Only 45 characters are valid and must be checked beforehand.
+ * @param {string} data Alphanumeric data to encode, valid characters are `0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:`
+ * @returns {EncodedData} The encoded data for all versions.
+ */
 function encodeAlphanumeric(data: string): EncodedData {
   const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:'
   const encoding = Encoding.Alphanumeric
   const length = data.length
   const bits: number[] = []
-  const result = (): EncodedData => ({ data1: [], data10: [], data27: [] })
+  const result = (): EncodedData => ({
+    DataVersionLow: [],
+    DataVersionMid: [],
+    DataVersionHigh: [],
+  })
 
+  // encode 2 characters at once
+  // 2 charaters are 11 bits, the last character is 6 bits
   for (let i = 0; i < length; i += 2) {
     let size = 6
     let value = charset.indexOf(data[i])
@@ -47,33 +84,57 @@ function encodeAlphanumeric(data: string): EncodedData {
     pushBits(bits, size, value)
   }
 
-  result.data27 = getData(encoding, length, 13, bits)
+  result.DataVersionHigh = getData(encoding, length, 13, bits)
   if (length < 2048) {
-    result.data10 = getData(encoding, length, 11, bits)
+    result.DataVersionMid = getData(encoding, length, 11, bits)
   }
   if (length < 512) {
-    result.data1 = getData(encoding, length, 9, bits)
+    result.DataVersionLow = getData(encoding, length, 9, bits)
   }
   return result
 }
 
+/**
+ * Encodes numeric data.
+ * @param {string} data Numeric data to encode.
+ * @returns {EncodedData} The encoded data for all versions.
+ */
 function encodeNumeric(data: string): EncodedData {
   const encoding = Encoding.Numeric
   const length = data.length
   const bits: number[] = []
-  const result = (): EncodedData => ({ data1: [], data10: [], data27: [] })
+  const result = (): EncodedData => ({
+    DataVersionLow: [],
+    DataVersionMid: [],
+    DataVersionHigh: [],
+  })
 
-  result.data27 = getData(encoding, length, 14, bits)
+  // encode 3 characters at once with dynamic char size
+  for (let i = 0; i < length; i += 3) {
+    const s = data.substr(i, 3)
+    const b = Math.ceil((length * 10) / 3)
+    pushBits(bits, b, parseInt(s, 10))
+  }
+
+  result.DataVersionHigh = getData(encoding, length, 14, bits)
   if (length < 4096) {
-    result.data10 = getData(encoding, length, 12, bits)
+    result.DataVersionMid = getData(encoding, length, 12, bits)
   }
   if (length < 1024) {
-    result.data1 = getData(encoding, length, 10, bits)
+    result.DataVersionLow = getData(encoding, length, 10, bits)
   }
   return result
 }
 
+/**
+ * Encode a single URL (experimental).
+ * The protocol and hostname (e.g. `https://github.com/`) are treated like alphanumeric data to
+ * save space, the following path is treated like binary data.
+ * @param {string} data The URL to encode.
+ * @returns {EncodedData} The encoded data for all versions.
+ */
 function encodeUrl(data: string): EncodedData {
+  console.log('URL')
   const slash = data.indexOf('/', 8) + 1 || data.length
   const result = encode(data.slice(0, slash).toUpperCase(), false)
 
@@ -83,21 +144,28 @@ function encodeUrl(data: string): EncodedData {
 
   const pathResult = encode(data.slice(slash), false)
 
-  result.data27 = result.data27.concat(pathResult.data27)
-  if (result.data10 && pathResult.data10) {
-    result.data10 = result.data10.concat(pathResult.data10)
+  result.DataVersionHigh = result.DataVersionHigh.concat(pathResult.DataVersionHigh)
+  if (result.DataVersionMid && pathResult.DataVersionMid) {
+    result.DataVersionMid = result.DataVersionMid.concat(pathResult.DataVersionMid)
   }
-  if (result.data1 && pathResult.data1) {
-    result.data1 = result.data1.concat(pathResult.data1)
+  if (result.DataVersionLow && pathResult.DataVersionLow) {
+    result.DataVersionLow = result.DataVersionLow.concat(pathResult.DataVersionLow)
   }
   return result
 }
 
+/**
+ * Encodes data using a method that fits the data type.
+ * @param {string|number|Buffer|number[]} data The data to encode.
+ * @param {boolean} parseUrl Flag wheter to optimize the resulting data for URLs.
+ * @returns {EncodedData} The encoded data for all versions.
+ */
 export function encode(data: string | number | Buffer | number[], parseUrl: boolean): EncodedData {
   let str: string
   let buf: Buffer
   const t = typeof data
 
+  // prepare the data
   if (t === 'string' || t === 'number') {
     str = `${data}`
     buf = Buffer.from(str)
@@ -111,6 +179,7 @@ export function encode(data: string | number | Buffer | number[], parseUrl: bool
     throw new Error('Bad data')
   }
 
+  // select the correct method, error out if limits are exceeded
   if (/^[0-9]+$/.test(str)) {
     if (buf.length > 7089) throw new Error('Too much data')
     return encodeNumeric(str)
--- packages/matrix/src/main.ts
+++ packages/matrix/src/main.ts
@@ -1,7 +1,7 @@
 import { encode } from './encode'
 import { calculateEc } from './errorcode'
 import { getMatrix } from './matrix'
-import { EcLevel, QRData, EncodedData } from './datatypes'
+import { EcLevel, QRData, EncodedData, Versions } from './datatypes'
 
 export { EcLevel }
 
@@ -9,52 +9,7 @@ export type QRVersionList = {
   [key in keyof typeof EcLevel]?: QRData
 }
 
-const versions = [
-  [], // there is no version 0
-  // total number of codewords, (number of ec codewords, number of blocks) * ( L, M, Q, H )
-  [26, 7, 1, 10, 1, 13, 1, 17, 1],
-  [44, 10, 1, 16, 1, 22, 1, 28, 1],
-  [70, 15, 1, 26, 1, 36, 2, 44, 2],
-  [100, 20, 1, 36, 2, 52, 2, 64, 4],
-  [134, 26, 1, 48, 2, 72, 4, 88, 4], // 5
-  [172, 36, 2, 64, 4, 96, 4, 112, 4],
-  [196, 40, 2, 72, 4, 108, 6, 130, 5],
-  [242, 48, 2, 88, 4, 132, 6, 156, 6],
-  [292, 60, 2, 110, 5, 160, 8, 192, 8],
-  [346, 72, 4, 130, 5, 192, 8, 224, 8], // 10
-  [404, 80, 4, 150, 5, 224, 8, 264, 11],
-  [466, 96, 4, 176, 8, 260, 10, 308, 11],
-  [532, 104, 4, 198, 9, 288, 12, 352, 16],
-  [581, 120, 4, 216, 9, 320, 16, 384, 16],
-  [655, 132, 6, 240, 10, 360, 12, 432, 18], // 15
-  [733, 144, 6, 280, 10, 408, 17, 480, 16],
-  [815, 168, 6, 308, 11, 448, 16, 532, 19],
-  [901, 180, 6, 338, 13, 504, 18, 588, 21],
-  [991, 196, 7, 364, 14, 546, 21, 650, 25],
-  [1085, 224, 8, 416, 16, 600, 20, 700, 25], // 20
-  [1156, 224, 8, 442, 17, 644, 23, 750, 25],
-  [1258, 252, 9, 476, 17, 690, 23, 816, 34],
-  [1364, 270, 9, 504, 18, 750, 25, 900, 30],
-  [1474, 300, 10, 560, 20, 810, 27, 960, 32],
-  [1588, 312, 12, 588, 21, 870, 29, 1050, 35], // 25
-  [1706, 336, 12, 644, 23, 952, 34, 1110, 37],
-  [1828, 360, 12, 700, 25, 1020, 34, 1200, 40],
-  [1921, 390, 13, 728, 26, 1050, 35, 1260, 42],
-  [2051, 420, 14, 784, 28, 1140, 38, 1350, 45],
-  [2185, 450, 15, 812, 29, 1200, 40, 1440, 48], // 30
-  [2323, 480, 16, 868, 31, 1290, 43, 1530, 51],
-  [2465, 510, 17, 924, 33, 1350, 45, 1620, 54],
-  [2611, 540, 18, 980, 35, 1440, 48, 1710, 57],
-  [2761, 570, 19, 1036, 37, 1530, 51, 1800, 60],
-  [2876, 570, 19, 1064, 38, 1590, 53, 1890, 63], // 35
-  [3034, 600, 20, 1120, 40, 1680, 56, 1980, 66],
-  [3196, 630, 21, 1204, 43, 1770, 59, 2100, 70],
-  [3362, 660, 22, 1260, 45, 1860, 62, 2220, 74],
-  [3532, 720, 24, 1316, 47, 1950, 65, 2310, 77],
-  [3706, 750, 25, 1372, 49, 2040, 68, 2430, 81], // 40
-]
-
-const versionList = versions.map((v, index) => {
+const versionList = Versions.map((v, index) => {
   if (!index) return {}
 
   const result: QRVersionList = {}
@@ -88,8 +43,8 @@ export function getTemplate(message: EncodedData, ecLevel: EcLevel): QRData {
   let i = 1
   let len = 0
 
-  if (message.data1) {
-    len = Math.ceil(message.data1.length / 8)
+  if (message.DataVersionLow) {
+    len = Math.ceil(message.DataVersionLow.length / 8)
   } else {
     i = 10
   }
@@ -100,8 +55,8 @@ export function getTemplate(message: EncodedData, ecLevel: EcLevel): QRData {
     }
   }
 
-  if (message.data10) {
-    len = Math.ceil(message.data10.length / 8)
+  if (message.DataVersionMid) {
+    len = Math.ceil(message.DataVersionMid.length / 8)
   } else {
     i = 27
   }
@@ -112,7 +67,7 @@ export function getTemplate(message: EncodedData, ecLevel: EcLevel): QRData {
     }
   }
 
-  len = Math.ceil(message.data27.length / 8)
+  len = Math.ceil(message.DataVersionHigh.length / 8)
   for (; /* i */ i < 41; i++) {
     const version = versionList[i][ecLevel]
     if (version && version.dataLength >= len) {
@@ -127,11 +82,11 @@ export function fillTemplate(message: EncodedData, template: QRData): QRData {
   let msg: number[]
 
   if (template.version < 10) {
-    msg = message.data1
+    msg = message.DataVersionLow
   } else if (template.version < 27) {
-    msg = message.data10
+    msg = message.DataVersionMid
   } else {
-    msg = message.data27
+    msg = message.DataVersionHigh
   }
 
   const len = msg.length
